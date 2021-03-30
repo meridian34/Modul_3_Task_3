ac8eda33aceb050de21ceaeb3ed365f89bcc1030
Sobol Andrey <sobol.andrey.25@gmail.com>
2021-03-30 12:51:00 +0300

    Добавьте файлы проекта.

---
+++ Modul_3_Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul_3_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
